3f8395107bf39e23e94d50953f8a2dde4ab3ad81
Mehmet Salih ATKAN <mehmetsalihatkan@gmail.com>
2021-12-23 13:02:27 +0300

    Dependencıy Injection& IdentityCore

--- ItServiceApp/Controllers/HomeController.cs
+++ ItServiceApp/Controllers/HomeController.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ItServiceApp.InjectOrnek;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ItServiceApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMyDependency _myDependency;
+
+        public HomeController(IMyDependency myDependency)
+        {
+            _myDependency = myDependency;
+        }
+
         public IActionResult Index()
         {
+            _myDependency.Log("Home/Index'e girildi");
             return View();
         }
     }
